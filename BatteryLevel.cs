335c3b08b6fbffd922b8a9f1b106c98e9486aa5d
RaghavendraSR06 <raghu99459@gmail.com>
2021-03-08 20:41:00 +0530

    Update BatteryLevel.cs

--- BatteryLevel.cs
+++ BatteryLevel.cs
@@ -2,32 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Checker
+namespace BatteryManagementSystem
 {
     class BatteryLevel
     {
-       public enum BatteryCondition
-       {
-           Low,
-           Normal,
-           High
-       }
+        public enum BatteryCondition
+        {
+            Low,
+            Normal,
+            High
+        }
 
-       public static BatteryCondition CheckBatteryCondition(float currentValue, float minValue, float maxValue)
-       {
-           if (currentValue > maxValue)
-           {
-               return BatteryCondition.High;
-               
-           }
+        public static BatteryCondition CheckBatteryCondition(float currentValue, float minValue, float maxValue)
+        {
+            if (CheckMaxCondition(currentValue, maxValue) == BatteryCondition.High)
+            {
+                return BatteryCondition.High;
+            }
 
-           if (currentValue < minValue)
-           {
-               return BatteryCondition.Low;
+            if (CheckMinCondition(currentValue, minValue) == BatteryCondition.Low)
+            {
+                return BatteryCondition.Low;
 
-           }
+            }
 
-           return BatteryCondition.Normal;
-       }
+            return BatteryCondition.Normal;
+        }
+
+        private static BatteryCondition CheckMaxCondition(float currentValue, float maxValue)
+        {
+            return currentValue > maxValue ? BatteryCondition.High : BatteryCondition.Normal;
+        }
+
+        private static BatteryCondition CheckMinCondition(float currentValue, float minValue)
+        {
+            return currentValue < minValue ? BatteryCondition.Low : BatteryCondition.Normal;
+        }
+
     }
 }
