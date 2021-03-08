2e432f0592ea155590341d8c85e452ebf88094e1
RaghavendraSR06 <raghu99459@gmail.com>
2021-03-08 20:37:42 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,10 +1,12 @@
-namespace Checker
+namespace BatteryManagementSystem
 {
     class Battery
     {
         static int Main()
         {
-            BatteryStatus.CheckBatteryCondition(40, 60, 0.4f);
+            BatteryStatus.CheckBatteryStatus(40, 60, 0.4f);
+            BatteryStatus.CheckBatteryStatus(-1, 20, 0.3f);
+            BatteryStatus.CheckBatteryStatus(100, -20, 0.1f);
             return 0;
         }
     }
